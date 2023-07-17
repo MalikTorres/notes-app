f45a0dfb41a308e0f4da98261f346531ec19f0cd
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-07-17 18:05:47 -0400

    outlining initial styles and components

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+// View -> container that will wrap multiple components together
+// text - to showcase any text
+// textinput -> component
+// button -> showcase any button
 
 export default function App() {
+
+  // function handleOnChangeText(getEnteredText) {
+  //   console.log(getEnteredText)
+  // }
+
+  // function handleOnPressButton() {
+
+  // }
+  //onChangeText={handleOnChangeText}
+  //onPress={handleOnPressButton}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      padding: 60
+    }}>
+      {/* to render input along with button */}
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='Add your Note Here' />
+        <Button style={styles.button} title='Add Note' />
+      </View>
+      {/* to render input along with button */}
+      {/* to render all the notes we created */}
+      <View style={styles.listContainer}>
+        <Text>Show Lists Here !</Text>
+      </View>
+
+      {/* to render all the notes we created */}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  inputContainer: {
+    flexDirection: 'row',
+    paddingBottom: 30,
+    boredrBottomWidth: 1
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    flex: 1
+  },
+  listContainer: {
+    paddingTop: 30
+  }
 });
